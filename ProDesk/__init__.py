c4e31593d647165abf9e2feed56c9f863a84fda3
Ramrachai <ramrachaim@gmail.com>
2020-05-15 00:33:00 +0600

    procfile fixed: changed application to run

--- ProDesk/__init__.py
+++ ProDesk/__init__.py
@@ -3,10 +3,14 @@ from flask_sqlalchemy import SQLAlchemy
 from flask_bcrypt import Bcrypt
 from flask_login import LoginManager
 from flask_mail import Mail
+import os 
 
 app = Flask(__name__)
 app.config['SECRET_KEY'] = "c96418b075871ec41adaea54fee6f4ac"
-app.config['SQLALCHEMY_DATABASE_URI'] = 'postgres://eyjbdhslylxxcw:2537e4a98d96a2fb95dd659505126f8a23b469000a1b130d68a8ea5493e3ae93@ec2-54-157-78-113.compute-1.amazonaws.com:5432/d3s5ievnk6355i'
+app.config['SQLALCHEMY_DATABASE_URI'] = os.getenv("DATABASE_URL") 
+
+# 'postgres://eyjbdhslylxxcw:2537e4a98d96a2fb95dd659505126f8a23b469000a1b130d68a8ea5493e3ae93@ec2-54-157-78-113.compute-1.amazonaws.com:5432/d3s5ievnk6355i'
+
 db = SQLAlchemy(app)
 bcrypt = Bcrypt(app)
 login_manager = LoginManager(app)
